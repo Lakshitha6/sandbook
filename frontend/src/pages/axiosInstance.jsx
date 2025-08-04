744083eb33a48c4c510e59a7d94bc34217d4f7fe
Lakshitha 69 <148607270+Lakshitha6@users.noreply.github.com>
2025-08-04 21:17:16 +0530

    change axiosInstance base url

--- frontend/src/pages/axiosInstance.jsx
+++ frontend/src/pages/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://2d8042b8-0a21-4a70-a1e4-332303ca9cde-dev.e1-us-east-azure.choreoapis.dev/sandbook-social-media/backend/v1',
 });
 
 axiosInstance.interceptors.request.use(
